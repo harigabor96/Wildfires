015be2169977f46c67ca63d94818826185dbc04c
Hári Gábor <gabor.hari@ecobit.hu>
2022-09-11 14:25:50 +0200

    .gitignore modified

--- WildfiresETL/src/main/scala/org/wildfires/pipeline/bronze/wildfire/Wildfire.scala
+++ WildfiresETL/src/main/scala/org/wildfires/pipeline/bronze/wildfire/Wildfire.scala
@@ -1,5 +1,22 @@
 package org.wildfires.pipeline.bronze.wildfire
 
-object Wildfire {
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.DataFrame
+import org.wildfires.pipeline.GenericPipeline
 
+case class Wildfire(spark: SparkContext) extends GenericPipeline {
+
+  override def extract(): Any = {
+    spark.
+
+  }
+
+  override def transform(extractedData: Any): DataFrame = {
+    val extractedDf = extractedData.asInstanceOf[DataFrame]
+    extractedDf
+  }
+
+  override def load(transformedData: DataFrame): Unit = {
+    transformedData.show()
+  }
 }
