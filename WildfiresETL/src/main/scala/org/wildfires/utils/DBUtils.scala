a6cf031c11f111fab47a00f8246f9c844439adb5
Hári Gábor <gabor.hari@ecobit.hu>
2022-09-24 17:29:10 +0200

    DBUtils unit test and exceptions

--- WildfiresETL/src/main/scala/org/wildfires/utils/DBUtils.scala
+++ WildfiresETL/src/main/scala/org/wildfires/utils/DBUtils.scala
@@ -6,12 +6,36 @@ import org.apache.spark.sql.SparkSession
 object DBUtils {
 
   def createDatabaseIfNotExist(spark: SparkSession, databaseName: String) = {
+    databaseName match {
+      case null => throw new Exception("Database Name cannot be null!")
+      case "" => throw new Exception("Database Name cannot be empty!")
+      case _ =>
+    }
+
     spark.sql(s"""
       CREATE DATABASE IF NOT EXISTS $databaseName
     """)
   }
 
   def createTableFromPath(spark: SparkSession, databaseName: String, tableName: String, deltaLocation: String) = {
+    databaseName match {
+      case null => throw new Exception("Database Name cannot be null!")
+      case "" => throw new Exception("Database Name cannot be empty!")
+      case _ =>
+    }
+
+    tableName match {
+      case null => throw new Exception("Table Name cannot be null!")
+      case "" => throw new Exception("Table Name cannot be empty!")
+      case _ =>
+    }
+
+    deltaLocation match {
+      case null => throw new Exception("Delta location cannot be null!")
+      case "" => throw new Exception("Delta location cannot be empty!")
+      case _ =>
+    }
+
     spark.sql(s"""
             CREATE TABLE $databaseName.$tableName
             USING DELTA
@@ -20,10 +44,34 @@ object DBUtils {
   }
 
   def optimizeTable(spark: SparkSession, databaseName: String, tableName: String) = {
+    databaseName match {
+      case null => throw new Exception("Database Name cannot be null!")
+      case "" => throw new Exception("Database Name cannot be empty!")
+      case _ =>
+    }
+
+    tableName match {
+      case null => throw new Exception("Table Name cannot be null!")
+      case "" => throw new Exception("Table Name cannot be empty!")
+      case _ =>
+    }
+
     DeltaTable.forName(spark,s"$databaseName.$tableName").optimize().executeCompaction()
   }
 
   def vacuumTable(spark: SparkSession, databaseName: String, tableName: String) = {
+    databaseName match {
+      case null => throw new Exception("Database Name cannot be null!")
+      case "" => throw new Exception("Database Name cannot be empty!")
+      case _ =>
+    }
+
+    tableName match {
+      case null => throw new Exception("Table Name cannot be null!")
+      case "" => throw new Exception("Table Name cannot be empty!")
+      case _ =>
+    }
+
     DeltaTable.forName(spark,s"$databaseName.$tableName").vacuum()
   }
 }
--- WildfiresETL/src/test/scala/testsuites/org/wildfires/utils/Utils_DBUtils.scala
+++ WildfiresETL/src/test/scala/testsuites/org/wildfires/utils/Utils_DBUtils.scala
@@ -1,16 +1,41 @@
 package testsuites.org.wildfires.utils
 
+import org.apache.spark.sql.DataFrame
 import testutils._
 import org.scalatest._
+import org.wildfires.utils.DBUtils._
 
 class Utils_DBUtils extends FunSuite with SharedSparkSession {
 
-  //createDatabaseIfNotExist()
+  test("createDatabaseIfNotExist()") {
+    assertThrows[Exception](createDatabaseIfNotExist(spark,null))
+    assertThrows[Exception](createDatabaseIfNotExist(spark,""))
+    assertThrows[Exception](createDatabaseIfNotExist(null,"database"))
+  }
 
-  //createTableFromPath()
+  test("createTableFromPath()") {
+    assertThrows[Exception](createTableFromPath(null, "database", "table", "delta"))
+    assertThrows[Exception](createTableFromPath(spark, null, "table", "delta"))
+    assertThrows[Exception](createTableFromPath(spark, "database", null, "delta"))
+    assertThrows[Exception](createTableFromPath(spark, "database", "table", null))
+    assertThrows[Exception](createTableFromPath(spark, "", "table", "delta"))
+    assertThrows[Exception](createTableFromPath(spark, "database", "", "delta"))
+    assertThrows[Exception](createTableFromPath(spark, "database", "table", ""))
+  }
 
-  //optimizeTable()
+  test("optimizeTable()") {
+    assertThrows[Exception](optimizeTable(null, "database", "table"))
+    assertThrows[Exception](optimizeTable(spark, null, "table"))
+    assertThrows[Exception](optimizeTable(spark, "database", null))
+    assertThrows[Exception](optimizeTable(spark, "", "table"))
+    assertThrows[Exception](optimizeTable(spark, "database", ""))
+  }
 
-  //vacuumTable()
-
+  test("vacuumTable()") {
+    assertThrows[Exception](vacuumTable(null, "database", "table"))
+    assertThrows[Exception](vacuumTable(spark, null, "table"))
+    assertThrows[Exception](vacuumTable(spark, "database", null))
+    assertThrows[Exception](vacuumTable(spark, "", "table"))
+    assertThrows[Exception](vacuumTable(spark, "database", ""))
+  }
 }
